d87633c014b87c3cbabeac5ff4f80cfe183cb622
Forrest Hawes <forestal@uw.edu>
2022-10-09 15:55:28 -0600

    learning about comments and string quoting(triple)

---
+++ chap3/comments_example.py
@@ -0,0 +1,5 @@
+# assign the text we want in our message to variable called 'message' -- note use of triple quoting
+message = """Hello Python programmers,
+ experts and novices all."""
+# Say hello to everyone.
+print(message)
