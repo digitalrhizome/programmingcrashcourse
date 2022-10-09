f782c8d060b344613b3bd4a17714cc665e891b3a
Forrest Hawes <forestal@uw.edu>
2022-10-09 13:57:06 -0600

    oops. I forgot to add brackets around the label(variable)

--- famous_person_quote.py
+++ famous_person_quote.py
@@ -1,3 +1,3 @@
 quote = "A person who never made a mistake never learned anything new."
-attribute_quote = f"Albert Einstein once said, \"quote\""
+attribute_quote = f"Albert Einstein once said, \"{quote}\""
 print(attribute_quote)
