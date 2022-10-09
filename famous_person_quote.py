b9724c160181def7609ad02e590aeca892fc540f
Forrest Hawes <forestal@uw.edu>
2022-10-09 13:59:11 -0600

    stored name of famous person in variable of same name

--- famous_person_quote.py
+++ famous_person_quote.py
@@ -1,3 +1,4 @@
 quote = "A person who never made a mistake never learned anything new."
-attribute_quote = f"Albert Einstein once said, \"{quote}\""
-print(attribute_quote)
+famous_person = "Albert Einstein"
+message = f"{famous_person} once said, \"{quote}\""
+print(message)
