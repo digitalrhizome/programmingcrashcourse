f75cebd3eef46d5f3a6be8e5ad1504d19d395274
Forrest Hawes <forestal@uw.edu>
2022-10-09 13:47:24 -0600

    doing exercises at end of chapter 2, exercise 2-3 and 2-4

---
+++ name_cases.py
@@ -0,0 +1,6 @@
+myname = "Forrest Hawes"
+
+message =  f"{myname}"
+print(message.lower())
+print(message.upper())
+print(message.title())
---
+++ personal_message.py
@@ -0,0 +1,4 @@
+myname = "Forrest Hawes"
+question = "Would you like to learn some Python today?"
+greeting = f"Hello, {myname}, {question}"
+print(greeting)
