b6bd0da21027e4c630ecb30aa980e821d7655e2d
Forrest Hawes <forestal@uw.edu>
2022-10-09 16:32:24 -0600

    continuing to add formatting with print calls and modular components (lol)

--- chap3/namelists.py
+++ chap3/namelists.py
@@ -1,13 +1,22 @@
 # store a list of friend's names. for now, we will hardcode it. 
 friends = ["David", "Kyle", "Chris"]
-print(friends[0])
-print(friends[1])
-print(friends[2])
+#print(friends[0])
+#print(friends[1])
+#print(friends[2])
 friends.append("KTara")
-print(friends[3])
+#print(friends[3])
+
+# use a for loop. not introduced in text, but i already know some Python.:-)
+intro = f"We are doing to print a greeting to each person in my list of friends:"
+blank_line = ""
+print(blank_line)
+print(intro)
+print(blank_line)
 for friend in friends:
-    message = f"Hello, {friends[0]}"
+    message = f"Hello, {friend}"
     print(message)
+
+print(blank_line)
 
 
 
