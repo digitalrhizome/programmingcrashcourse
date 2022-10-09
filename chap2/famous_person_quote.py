3bb9d317f239845b01afb5dcd8e8c8dfc74e0a71
Forrest Hawes <forestal@uw.edu>
2022-10-09 16:01:25 -0600

    adding some extra features to the famous person program

--- chap2/famous_person_quote.py
+++ chap2/famous_person_quote.py
@@ -1,4 +1,12 @@
+# This program will store the quote and its famous author  in memory, then assemble a message and print that to std.output
+# assign the quote to a variable
 quote = "A person who never made a mistake never learned anything new."
 famous_person = "Albert Einstein"
+blank_line = "\n"
+screen_extras = "*" * 40
+print(blank_line)
+print(screen_extras)
 message = f"{famous_person} once said, \"{quote}\""
 print(message)
+print(screen_extras)
+print(blank_line)
