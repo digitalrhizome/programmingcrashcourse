42f5174451747460dece423e6eb0f32a7c081938
Forrest Hawes <forestal@uw.edu>
2022-10-09 20:21:30 -0600

    sorting cars...

---
+++ chap3/cars.py
@@ -0,0 +1,17 @@
+# let's store a list of cars in the list. 
+
+carlist = ['bmw', 'audi', 'toyota', 'subaru']
+print("here is the original list: ")
+print(carlist)
+# let's sort the list
+#cars.sort()
+# let's sort the list in reverse order
+#carlist.sort(reverse=True)
+print("\nHere is the sorted list: ")
+print(sorted(carlist))
+print("\nhere is the original list: ")
+print(carlist)
+
+# let's compare to the use of the reverse method
+carlist.reverse()
+print(carlist)
