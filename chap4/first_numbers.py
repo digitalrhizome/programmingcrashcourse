733a1c3c2d1ba32f4b55315335abec60a40a39ae
Forrest Hawes <forestal@uw.edu>
2022-10-10 19:38:54 -0600

    giving range() function only one value, then it starts at zero

--- chap4/first_numbers.py
+++ chap4/first_numbers.py
@@ -1,3 +1,11 @@
+# demonstrating the use of the range() function in python3:
+
 for value in range(1,6):
+    print(value)
+
+print("")
+# If we give the range() function only one argument, then it starts at 0:
+
+for value in range(6):
     print(value)
 
