86d5c6a927978823b78a2dfd95eb3f7a66e10f0e
Forrest Hawes <forestal@uw.edu>
2022-10-10 19:51:27 -0600

    practicing using range() function to generate a specific list of numbers

---
+++ chap4/count_to_20.py
@@ -0,0 +1,7 @@
+# use a for loop to count to 20
+for value in range(21):
+    print(value)
+print("But you see, that doesn't do what the assignment said, since it starts at zero. Try again with another method:")
+# we can also do it like this:
+for value in range(1,21):
+    print(value)
