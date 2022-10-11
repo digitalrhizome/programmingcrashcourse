bfc00da2757d235cf787b9c703b24ed6dd085753
Forrest Hawes <forestal@uw.edu>
2022-10-10 19:33:37 -0600

    animals which would (not) make great pets!

---
+++ chap4/animals.py
@@ -0,0 +1,7 @@
+# store the names of animals in a list
+animals = ["penguin", 'giraffe', 'emu', 'zebra', 'leopard', 'anteater', 'blue whale']
+for animal in animals:
+    print(f"{animal} would make a great pet")
+
+print("Any of these animals would make a great pet!")
+print("Obviously, this statement is ridiculous, but the exercise directed me to write this!")
