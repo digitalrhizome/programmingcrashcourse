2de64178251c6f2f40b5a792be96bbccebb330b0
Forrest Hawes <forestal@uw.edu>
2022-10-10 19:28:09 -0600

    finishing the ridiculous i love pizza statements in exercise 4-1

--- chap4/pizzas.py
+++ chap4/pizzas.py
@@ -3,5 +3,7 @@ pizzas = ["pepperoni", "pineapple", "cheese"]
 print(pizzas)
 for pizza in pizzas:
     print(f"I like {pizza} pizza")
-
-    
+print("Well, sometimes I really like pizza!")
+print("And sometimes it is just right!")  
+print("And sometimes I eat pizza with friends, too!")
+print("I really love pizza!")
