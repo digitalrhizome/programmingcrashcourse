0e3201d7c29c1d2f85ec6917d4c8f52b78136270
Forrest Hawes <forestal@uw.edu>
2022-10-10 19:45:02 -0600

    make the squares program more concise

--- chap4/squares.py
+++ chap4/squares.py
@@ -3,7 +3,9 @@
 # initialize an empty list, assign it to the variable
 squares = []
 for value in range(1,11):
-    square = value ** 2
-    squares.append(square)
+    # we can make the program more concise by eliminating the temporary variable 'square'
+    #square = value ** 2
+    # and append each value directly to the list:
+    squares.append(value**2)
 # print the resulting list, it should print all the squares between 1 and 100, inclusive:
 print(squares)
