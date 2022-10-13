93a0065f6e9f2601926457e701f491cee185f258
Forrest Hawes <forestal@uw.edu>
2022-10-12 22:24:40 -0600

    learning the step parameter to the range() function builtin.

---
+++ chap4/odd_numbers.py
@@ -0,0 +1,8 @@
+message = """We printing a list of numbers, starting with 1 and ending at 20, but we are only printing the odd numbers.
+We use a for loop and the step parameter to the range() function."""
+
+print(message)
+
+for value in range(1,21,2):
+    print(value)
+
