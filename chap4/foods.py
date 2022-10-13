4d2f052845075cd6e6ce29a52727c2541bc91f30
Forrest Hawes <forestal@uw.edu>
2022-10-12 22:44:38 -0600

    exercise 4-10 at end of file

--- chap4/foods.py
+++ chap4/foods.py
@@ -10,3 +10,7 @@ print(my_foods)
 print("\nMy friend's favorite foods are: ")
 print(friend_foods)
 
+# for exercise 4-10
+print("The first three items in the list are: ", my_foods[:3])
+print("Three items from the middle of the list are: ", my_foods[1:4])
+print("The last three items from the list are: ", my_foods[-3:])
